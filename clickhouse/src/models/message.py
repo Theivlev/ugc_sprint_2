506b9230822735e4020be5b11d4cbb6efdc13bf9
Ivlev Alexey <theivlev@yandex.ru>
2025-03-31 17:48:21 +0400

    Ключи

--- clickhouse/src/models/message.py
+++ clickhouse/src/models/message.py
@@ -19,12 +19,12 @@ class MessageDTO(UUIDMixin):
         Создает объект MessageDTO из сообщения Kafka.
         """
         try:
-            deсode_value = json.loads(message['value'].decode('utf-8'))
+            deсode_value: dict = json.loads(message['value'].decode('utf-8'))
 
             user_id = deсode_value.get('user_id')
             movie_id = deсode_value.get('movie_id')
-            action = message['key'].decode('utf-8')
-            event_data = message['value'].decode('utf-8')
+            action = message.get('key', b'').decode('utf-8')
+            event_data = message.get('value', b'').decode('utf-8')
 
             user_id = UUID(user_id) if isinstance(user_id, str) else user_id
             movie_id = UUID(movie_id) if isinstance(movie_id, str) else movie_id
