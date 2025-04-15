cda8aed576a2e9f455e6fcf9d258792f36771978
Ivlev Alexey <theivlev@yandex.ru>
2025-04-15 11:15:34 +0400

    Начал дополнять core

---
+++ ugc_fastapi/src/core/config.py
@@ -0,0 +1,26 @@
+from logging import config as logging_config
+
+from pydantic import BaseSettings, SettingsConfigDict, MongoDsn, Field
+
+from .logger import LOGGING_CONFIG
+
+logging_config.dictConfig(LOGGING_CONFIG)
+
+
+class Settings(BaseSettings):
+    """Настраивает класс для чтения переменных окружения.."""
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
+    project_name: str
+    project_name: str
+    project_summary: str
+    project_version: str
+    project_terms_of_service: str
+
+    mongo_dsn: MongoDsn
+    mongo_db: str
+
+    auth_grpc_host: str
+    auth_grpc_port: int
+
+settings = Settings()
